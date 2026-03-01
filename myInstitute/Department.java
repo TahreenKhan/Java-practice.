c2d25c6d2b929403a1f1b7aefd1daaef89e74a1b
Tahreen Khan <Tahreenkhan2351@gmail.com>
2026-03-01 16:38:59 +0530

    tracking files

---
+++ myInstitute/Department.java
@@ -0,0 +1,11 @@
+package myInstitute;
+
+public class Department{
+    public void display(){
+        System.out.println("Staff: Aamdeep");
+        System.out.println("Staff: Ashtosh");
+        System.out.println("Staff: Golu");
+        System.out.println("Staff: kachibai");
+        System.out.println("Staff: leon");
+    }
+}

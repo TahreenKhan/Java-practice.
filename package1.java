7a18a90e03ad3d079353c92ae6181e662837e4ef
Tahreen Khan <Tahreenkhan2351@gmail.com>
2026-03-01 16:30:37 +0530

    java problem files

---
+++ package1.java
@@ -0,0 +1,10 @@
+import myInstitute.Department;
+
+public class package1 {
+    public static void main(String[] args){
+        Department d = new Department();
+        d.display();
+    }
+    
+}
+
